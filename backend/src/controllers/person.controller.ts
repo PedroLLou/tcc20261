18abc4848abfafdc219ddc77bcc9cb2c52d8073a
Frederick <frederickvitorino2@hotmail.com>
2026-04-07 19:52:20 -0300

    feat(database): integrar Prisma ORM e expandir rotas da API

--- backend/src/controllers/person.controller.ts
+++ backend/src/controllers/person.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Delete, Put } from '@nestjs/common';
 import { PersonService } from '../services/person.service';
 import { PersonDto } from '../dto/person.dto';
 
@@ -9,5 +9,25 @@ export class PersonController {
   @Post()
   async create(@Body() personDto: PersonDto) {
     return this.personService.create(personDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.personService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.personService.findOne(Number(id));
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() personDto: PersonDto) {
+    return this.personService.update(Number(id), personDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.personService.remove(Number(id));
   }
 }
--- backend/src/services/person.service.ts
+++ backend/src/services/person.service.ts
@@ -7,8 +7,24 @@ export class PersonService {
   constructor(private readonly personRepository: PersonRepository) {}
 
   async create(personDto: PersonDto) {
-    const exists = await this.personRepository.existsByName(personDto.name);
-    if (exists) throw new Error('Person already exists');
+    const exists = await this.personRepository.existsByEmail(personDto.email);
+    if (exists) throw new Error('Email already being used');
     return this.personRepository.create(personDto);
+  }
+
+  async findAll() {
+    return this.personRepository.findAll();
+  }
+
+  async findOne(id: number) {
+    return this.personRepository.findOne(id);
+  }
+
+  async update(id: number, personDto: PersonDto) {
+    return this.personRepository.update(id, personDto);
+  }
+
+  async remove(id: number) {
+    return this.personRepository.remove(id);
   }
 }
